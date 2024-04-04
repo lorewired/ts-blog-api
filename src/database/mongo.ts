8e738c22062aa4a07e367aaccf3ad4a3120bc5d9
nier704 <lorecode12@gmail.com>
2024-04-04 15:33:32 +0100

    feat: add mongo client with mongodb connection

---
+++ src/database/mongo.ts
@@ -0,0 +1,20 @@
+import { MongoClient as Mongo, Db } from 'mongodb'
+
+export const MongoClient = {
+    client: undefined as unknown as Mongo,
+    db: undefined as unknown as Db,
+
+    async connect(): Promise<void> {
+        const url = process.env.MONGODB_URL;
+        const username = process.env.MONGO_USER;
+        const password = process.env.MONGO_PASSWORD;
+        
+        const client = new Mongo(url, { auth: {username, password}});
+        const db = client.db("blog-db");
+
+        this.client = client;
+        this.db = db;
+
+        console.log("connected to mongodb")
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,38 @@
 import express from "express";
 import { config } from "dotenv";
 import { GetUserController } from "./controllers/get-messages/get-messages";
-import { MongoGetMessagesRepository, PostGresGetMessagesRepository } from "./repositories/get-messages/mongo-get-messages";
+import { MongoGetMessagesRepository } from "./repositories/get-messages/mongo-get-messages";
+import { MongoClient } from "./database/mongo";
 
 config();
 
-const app = express();
 const port = process.env.PORT || 8080;
 
-app.use((req, res, next) => {
-  const time = new Date(Date.now()).toString();
-  console.log(req.method, req.hostname, req.path, time);
-  next();
-});
+const main = async () => {
+  const app = express();
 
-app.get("/", (req, res) => {
-  res.send("welcome");
-});
+  await MongoClient.connect();
 
-app.get("/messages", async (req, res) => {
-  const mongoGetMessagesRepository = new MongoGetMessagesRepository();
-  const getMessagesController = new GetUserController(mongoGetMessagesRepository);
-  const { statusCode, body } = await getMessagesController.handle();
-  res.send(body).status(statusCode);
-});
+  app.use((req, res, next) => {
+    const time = new Date(Date.now()).toString();
+    console.log(req.method, req.hostname, req.path, time);
+    next();
+  });
+  
+  app.get("/", (req, res) => {
+    res.send("welcome");
+  });
+  
+  app.get("/messages", async (req, res) => {
+    const mongoGetMessagesRepository = new MongoGetMessagesRepository();
+    const getMessagesController = new GetUserController(mongoGetMessagesRepository);
+    const { statusCode, body } = await getMessagesController.handle();
+    res.send(body).status(statusCode);
+  });
 
-app.listen(port, () => {
-  console.clear();
-  console.log(`server listening on port ${port}\n`);
-});
+  app.listen(port, () => {
+    console.log(`listening on port: ${port}\n`);
+  });
+}
+
+main();
