eb94c61758d52958d94fba972081f9cbcf675f06
nier704 <lorecode12@gmail.com>
2024-04-12 11:59:54 +0100

    update issue

--- src/controllers/update-message/update-message.ts
+++ src/controllers/update-message/update-message.ts
@@ -16,17 +16,6 @@ export class UpdateMessageController implements IController {
          if (!id) return badRequest("id is required");
          if (!body) return badRequest("body is required");
 
-         const allowedToUpdate: (keyof UpdateMessageParams)[] = ["author", "text"];
-
-         const someFieldsIsNotAllowed = Object.keys(body)
-            .some(key => !allowedToUpdate.includes(key as keyof UpdateMessageParams));
-
-         if (someFieldsIsNotAllowed){
-            const notAllowedFields = Object.keys(body)
-                .filter(key => !allowedToUpdate.includes(key as keyof UpdateMessageParams));
-            return badRequest(`The following fields are not allowed to update: ${notAllowedFields.join(", ")}`);
-         }
-
          const message = await this.updateMessageRepository.updateMessage(id, body);
          
          return ok(message);
